9b843d641bcb59946dff20126fc43169e39c419b
Brett Porter <BrettPorter@Bretts-MacBook.attlocal.net>
2017-06-10 02:02:16 -0400

    Added Drinks Class
    -Added ability to create a drink object
    -Object will be used to determine and hold value for alcohol mass of
    drink

---
+++ Jingled/Drink.swift
@@ -0,0 +1,36 @@
+//
+//  Drink.swift
+//  Jingled
+//
+//  Created by Brett Porter on 6/10/17.
+//  Copyright © 2017 PorterApps. All rights reserved.
+//
+
+import Foundation
+
+// Holds alcohol content of a drink. Used to determine total alcohol in the drink. 
+// Right now only has calculations to support US standerd of measurements
+class Drink {
+    
+    private var _alcoholByVolume: Double! // Assumed to be taken in as a percentage
+    private var _volumeOfDrink: Double!   // Assumed to be taken in as ounces, for now; changed to liters
+    private var _alcoholMass: Double!     // Will be held in grams
+
+    init(alcoholByVolume: Double, volumeOfDrink: Double) {
+        _alcoholByVolume = alcoholByVolume
+        _volumeOfDrink = volumeOfDrink
+        _alcoholMass = calculateAlcoholMass()
+    }
+    
+    //Determines grams of pure alcohol in the drink
+    private func calculateAlcoholMass() -> Double {
+        let alcoholMassDensity = 23.341 // gram per fluid ounce
+        let alcoholMass = (_volumeOfDrink * _alcoholByVolume * alcoholMassDensity)
+        return alcoholMass
+    }
+    
+    var alcoholMass: Double {
+        return _alcoholMass
+    }
+    
+}
--- Jingled/ViewController.swift
+++ Jingled/ViewController.swift
@@ -12,14 +12,11 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let d1 = Drink(alcoholByVolume: 0.05, volumeOfDrink: 16)
+        print(d1.alcoholMass)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
 
