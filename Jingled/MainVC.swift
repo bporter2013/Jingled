91b918d61039a66f646c24c1f48e716c5d232e25
Brett Porter <BrettPorter@Bretts-MacBook.attlocal.net>
2017-06-18 14:34:29 -0400

    Began Work on View
    - Set up all basic outline for view controllers
    - Finished with Main View UI
    
    - Need to Do
    - Update UML Diagram
    - Create UML diagram for view controllers
    - Create the two views that will be displayed on launch until
    conditions are met.

--- Jingled/MainVC.swift
+++ Jingled/MainVC.swift
@@ -8,10 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+     
         
        
     }
